b14826bf12dd5262fd9ede5773d0e397a04e9baf
Bryan <bryanzin7@gmail.com>
2025-05-27 16:24:56 -0700

    wip

---
+++ calculator.js
@@ -0,0 +1,17 @@
+const bottomButtons = document.querySelector(".bottom-buttons");
+
+function createButtons(){
+    for(let row = 0; row < 21; row++){
+        const numericalButton = document.createElement("div");
+        numericalButton.setAttribute("class", "numerical-button");
+
+        // change color on hover
+        numericalButton.addEventListener("click", () => {
+            alert("Clicked");
+        });
+
+        bottomButtons.appendChild(numericalButton);
+    };
+}
+
+createButtons();
